e342535dc8791e177522d281f75f84dba4fb3f87
panda1 <blueblue0011@gmail.com>
2022-07-19 17:00:58 -0700

    on 6

---
+++ app.js
@@ -0,0 +1,7 @@
+cookies.set(`username`, `Iambestuser`);
+let click_event = document.getElementById(`clickhere`);
+
+click_event.addEventListener(`onclick`, button_clicks)
+function button_clicks (detail) {
+    
+}
