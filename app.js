5435bc4aa424c6cc3e3cb0ae336a4191a27af07a
panda1 <blueblue0011@gmail.com>
2022-07-19 18:23:57 -0700

    knowledge 2 not working

--- app.js
+++ app.js
@@ -8,3 +8,8 @@ click_event.addEventListener(`click`, button_clicks);
 function button_clicks (detail) {
     Cookies.set(`button_clicked`, `yes`);
 }
+
+let user_1 = Cookies.get(`button_clicked`);
+if(user_1 !== undefined) {
+    document.body.insertAdjacentHTML(`afterbegin`, `<h1> congratulation on baking a cookie</h1>`);
+}
